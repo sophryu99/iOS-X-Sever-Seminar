8b05d378382d952741f0dad31b2b8cf6e8324371
Juhyeoklee <gur6531@naver.com>
2020-05-30 16:38:31 +0900

    [CRATE] APIService directory

---
+++ iOS-Design-Project/iOS-Design-Project/Resource/APIService/APIConstants.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstants.swift
+//  iOS-Design-Project
+//
+//  Created by 이주혁 on 2020/05/30.
+//  Copyright © 2020 이주혁. All rights reserved.
+//
+
+import Foundation
+
+struct APIConstants {
+    static let baseURL = ""
+    
+    // URL Info
+}
